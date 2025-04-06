c87114336b7ab32442c212d881fedad0771dc3b0
João Teixeira <138622216+JTeixeiraz@users.noreply.github.com>
2025-04-06 18:38:31 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/calcularnotas/MainActivity.kt
+++ app/src/main/java/com/example/calcularnotas/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -23,7 +24,8 @@ class MainActivity : AppCompatActivity() {
         }
         
         var botao = findViewById<Button>(R.id.btn_Calcular);
-        botao.setOnClickListener { 
+        botao.setOnClickListener {
+            var result = findViewById<TextView>(R.id.txt_result);
             val nota1 = findViewById<EditText>(R.id.edt_Nota1);
             val valorNota1 = nota1.getText().toString().toDoubleOrNull()?:0.0;
             val nota2 = findViewById<EditText>(R.id.edt_Nota2);
@@ -31,6 +33,18 @@ class MainActivity : AppCompatActivity() {
             val nota3 = findViewById<EditText>(R.id.edt_Nota3);
             val valorNota3 = nota3.getText().toString().toDoubleOrNull()?:0.0;
             var resultado = (valorNota1+valorNota2+valorNota3)/3
+            if (resultado >=7){
+                if (resultado>=9){
+                   result.setText("Aluno aprovado com excelêncoa");
+                }
+                result.setText("Aluno aprovado");
+            }
+            if (resultado <=6){
+                if (resultado<=3){
+                    result.setText("Aluno reprovado sem direito a recuperação");
+                }
+                result.setText("Aluno reprovado com direito a recuperação");
+            }
             Toast.makeText(this, "Valor da media: ${resultado}", Toast.LENGTH_SHORT).show()
         }
     }
